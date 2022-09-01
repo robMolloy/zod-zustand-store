12f86b5af764da99827108f58f884d957be25a67
robmolloyntt <robert.molloy@nttdata.com>
2022-09-01 09:50:35 +0100

    parcel setup

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-
-'use strict'
-
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./zod-zustand-store.cjs.production.min.js')
-} else {
-  module.exports = require('./zod-zustand-store.cjs.development.js')
-}
